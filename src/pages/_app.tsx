d3db6cb23e363a3f33c8cc2206ad0a43e5c75522
michal_cfc <michal.jachimowski@gmail.com>
2023-04-19 15:06:41 +0200

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/index.scss';
 
 // seo
-import { DefaultSeo } from 'next-seo';
-import Script from 'next/script';
 import theme from '../theme';
 
 // Import Swiper styles
@@ -18,59 +16,46 @@ import { Layouts } from '@/components/common/Layouts';
 // types
 import { MyAppProps } from '@/components/common/Layouts/Layout.d';
 
+// hooks
+import {
+  SiteContext,
+  useSiteContext,
+} from '@/hooks/useSiteContext';
+
 // lib
 import { getAllMenus } from '@/lib/menus';
+import { getSiteMetadata } from '@/lib/site';
 
-// hooks
-import { SiteContext, useSiteContext } from '@/hooks/useSiteContext';
-import { Meta } from '@/components/Layout/components/Meta';
-
-const MyApp = ({ Component, pageProps, menus }: MyAppProps) => {
+const MyApp = ({
+  Component, pageProps, metadata, menus,
+}: MyAppProps) => {
   const Layout = Layouts[Component.Layout] ?? ((page) => page);
 
   const site = useSiteContext({
     menus,
+    metadata,
   });
 
-  console.log(site);
-
   return (
-    <>
-      <Meta />
-      <DefaultSeo
-        title="Portfolio"
-        description="My portfolio"
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: 'https://www.url.ie/',
-          site_name: 'Portfolio',
-        }}
-        twitter={{
-          handle: '@handle',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
-      />
-      <SiteContext.Provider value={site}>
-        <ChakraProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
-      </SiteContext.Provider>
-    </>
+    <SiteContext.Provider value={site}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </SiteContext.Provider>
   );
 };
 
 MyApp.getInitialProps = async function (appContext: AppContext) {
   const appProps = await NextApp.getInitialProps(appContext);
 
-  const { menus = [] } = await getAllMenus();
+  const { menus } = await getAllMenus();
 
   return {
     ...appProps,
     menus,
+    metadata: await getSiteMetadata(),
   };
 };
 
